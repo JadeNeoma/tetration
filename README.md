70f30c361cf77d939029357462fc519ddafdf46b
Jordyn Young <29105443+JordynY@users.noreply.github.com>
2020-06-01 17:44:45 +0100

    Updated various commands to make this work better with optional arguments, also included a better README and added functionality to find x

---
+++ README.md
@@ -0,0 +1,23 @@
+# Tetration
+So this is just a simple python script that calculates y = <sup>x</sup>K.
+This is done in a way that isn't packaged but can be easily imported into other projects. Tetration.py is safe to import, it can also be run from the commandline with the arguments x and k respectively, or it can just be run in which case it will prompt for x and k within the program.  
+
+## Use
+### Imported:
+* tetration.calc(x, k) is the same as tetration.calc_y(x, k)
+* tetration.calc_y(x, k) returns y
+* tetration.calc_x(y, k) returns x, tests between -5 and 5, excl. 0
+
+### Run from console:
+* Self-Explanatory, option 1 tests between -5 and 5 excl. 0
+
+### Run from Command Line:
+* takes two required arguments
+* defaults to calculating y assuming the required arguments are x and n respectively
+* can be forced to find x by ending with -x
+* can be forced to find y by ending with -y, included for completeness
+
+## Warning
+1. I haven't included a timeout, if you enter numbers to big for your computer to handle it will hang.
+2. x can be any integers apart from 0, beware negative numbers could result in complex results.
+3. k can be any integer greater than 0
--- tetration.py
+++ tetration.py
@@ -3,24 +3,67 @@
 # There is no timeout so be careful what you pass it
 
 import argparse
+from varname import nameof
 
 
-def calc(x, k):  # calculate y = x|k
+def find_k(y, x):
+    print("Not implemented")
+
+
+def calc_x(y, k):  # calculates x given y and k, linear search up from 1 to 5 and from -1 to -5
+    found = False
+    for x in range(1, 6):
+        if y == calc_y(x, k):
+            out = x
+            found = True
+            break
+        elif y == calc_y(x * -1, k):
+            out = x * -1
+            found = True
+            break
+    if found:
+        return x
+    else:
+        return None
+
+
+def calc_y(x, k):  # calculate y = x|k
     y = 1
-    if k >= 1:
-        y = x ** calc(x, k - 1)
+    if k < 0 or x == 0:
+        raise ValueError("x or k value not allowed")
+    elif k > 0:
+        y = x ** calc_y(x, k - 1)
     return y
+
+
+def console_converter(default, name, needed):
+    if needed:
+        assign = int(input("Please enter " + name + ": "))
+        return assign
+    else:
+        return default
+
+
+def calc(p, q):
+    return calc_y(p, q)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("x", type=int, nargs='?', const=0)
-    parser.add_argument("k", type=int, nargs='?', const=0)
+    parser.add_argument("num1", type=int, nargs='?', const=0)
+    parser.add_argument("num2", type=int, nargs='?', const=0)
+    parser.add_argument('-x', '--calculate_x', action='store_true', help="allows finding of x")
+    parser.add_argument('-y', '--calculate_y', action='store_true', help="allows finding of x")
     args = parser.parse_args()
-    if args.k is None:
-        args.x = int(input("x:"))
-        args.k = int(input("k:"))
-    print(calc(args.x, args.k))
+    needed = args.num2 is None
+    m = console_converter(0, "\n0 to calculate y given x & k\n1 to calculate x given y & k\n", needed)
+    if args.calculate_x or m == 1:
+        out = calc_x(console_converter(args.num1, "y", needed), console_converter(args.num2, "k", needed))
+    elif args.calculate_y or m == 0:
+        out = calc_y(console_converter(args.num1, "x", needed), console_converter(args.num2, "k", needed))
+    else:
+        out = calc(console_converter(args.num1, "x", needed), console_converter(args.num2, "k", needed))
+    print(out)
 
 
 if __name__ == "__main__":
